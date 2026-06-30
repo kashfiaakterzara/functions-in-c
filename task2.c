20ac50d7aa7765bcdb2c0a18cc04b0d77b99307f
kashfiaakterzara <kashfiaakter13@gmail.com>
2026-07-01 03:27:58 +0600

    Update task2.c

--- task2.c
+++ task2.c
@@ -6,15 +6,11 @@ int main(){
 int id, age, credit;
 double cgpa;
 
-printf("Enter Student ID Age CGPA Credits: ");
-
-scanf("%d %d %lf %d", &id, &age, &cgpa, &credit);
-
+printf("Enter Student ID Age Credits CGPA: ");
+scanf("%d %d %lf %d", &id, &age, &credit, &cgpa);
 print_student_profile(id, age, credit, cgpa);
 
 return 0;
-
-
 
 }
 
@@ -25,13 +21,7 @@ void print_student_profile(int id, int age, int credit, double cgpa)
     printf("=====================================\n");
     printf("ID Number         : %d\n", id);
     printf("Age               : %d years old\n", age);
-    printf("CUrrent CGPA      : %.2lf\n", cgpa);
     printf("Credits Finished  : %d\n", credit);
+    printf("CUrrent CGPA      : %.2lf\n", cgpa);
 
-
-  
-
-
-
-
-}
+  }
